4f8bad2b41abcdf196af5099bac4cc84d0a2828b
Petko <pavlovskipetko@abv.bg>
2021-04-05 12:45:19 +0300

    Get sample contract working.

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+
+import Web3 from "web3";
+import { address, ERC20TransferABI } from "./mocks/sampleContractABI";
+
+const web3 = new Web3("https://cloudflare-eth.com");
+const sampleContract = new web3.eth.Contract(ERC20TransferABI , address);
+console.log(sampleContract);
