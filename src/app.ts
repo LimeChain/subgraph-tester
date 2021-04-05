64f955a57883337974fcab88dc34242d406c76e2
Petko <pavlovskipetko@abv.bg>
2021-04-05 15:31:07 +0300

    Remove jest. Will try with sinon.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
-
+import { assert } from "chai";
 import Web3 from "web3";
 import { address, ERC20TransferABI } from "./mocks/sampleContractABI";
 
 const web3 = new Web3("https://cloudflare-eth.com");
-const sampleContract = new web3.eth.Contract(ERC20TransferABI , address);
-console.log("transfer" in sampleContract.methods);
+const sampleContract = new web3.eth.Contract(ERC20TransferABI, address);
+
+const mockContractFunction = (contract: any, functionName: string) => {
+  assert(
+    functionName in contract.methods,
+    "Function does not exist in contract.",
+  );
+  return contract.methods[functionName];
+};
+
+const res = mockContractFunction(sampleContract, "transfer");
+console.log(res);
