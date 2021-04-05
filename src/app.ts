3f13f44f1d0cfefa3b8dd89b13dc781b5beea483
Petko <pavlovskipetko@abv.bg>
2021-04-05 16:09:47 +0300

    Successfully mocking functions with sinon.

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,20 @@
 import { assert } from "chai";
+import sinon from "sinon";
 import Web3 from "web3";
 import { address, ERC20TransferABI } from "./mocks/sampleContractABI";
 
 const web3 = new Web3("https://cloudflare-eth.com");
 const sampleContract = new web3.eth.Contract(ERC20TransferABI, address);
 
-const mockContractFunction = (contract: any, functionName: string) => {
+const mockContractFunction = (contract: any, functionName: string, mockReturn: any) => {
   assert(
     functionName in contract.methods,
     "Function does not exist in contract.",
   );
-  return contract.methods[functionName];
+
+  const mockRes = sinon.mock().returns(mockReturn);
+  return mockRes();
 };
 
-const res = mockContractFunction(sampleContract, "transfer");
+const res = mockContractFunction(sampleContract, "transfer", "done");
 console.log(res);
