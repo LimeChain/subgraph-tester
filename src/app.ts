67fc5f17e538a150baf555fd38d2b15313c3b0c7
Petko <pavlovskipetko@abv.bg>
2021-04-05 14:00:13 +0300

    Switch to yarn.

--- src/app.ts
+++ src/app.ts
@@ -4,4 +4,4 @@ import { address, ERC20TransferABI } from "./mocks/sampleContractABI";
 
 const web3 = new Web3("https://cloudflare-eth.com");
 const sampleContract = new web3.eth.Contract(ERC20TransferABI , address);
-console.log(sampleContract);
+console.log("transfer" in sampleContract.methods);
