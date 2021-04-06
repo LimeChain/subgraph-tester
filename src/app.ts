dd916426214ea17139e4d19f4e936d1cbec6a948
Petko <pavlovskipetko@abv.bg>
2021-04-06 11:52:56 +0300

    Implement option withArgs for mocking functions.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,24 @@
 import { assert } from "chai";
 import sinon from "sinon";
 
+export interface IMockFunctionArgs {
+  // TODO: Create and use a Contract interface
+  contract: any;
+  fName: string;
+  mockReturn: () => {};
+  // TODO: convert this to map of args
+  withArgs?: any;
+}
+
+export interface IRunFunctionArgs {
+  fName: string;
+  // TODO: convert this to map of args
+  withArgs?: any;
+}
+
 const mockReturns: Map<string, any> = new Map();
 
-export const run = (fName: string) => {
+export const run = ({ fName }: IRunFunctionArgs) => {
   assert(
     mockReturns.get(fName) !== undefined,
     "This function has not yet been mocked.",
@@ -11,7 +26,7 @@ export const run = (fName: string) => {
   return mockReturns.get(fName)();
 };
 
-export const mock = (contract: any, fName: string, mockReturn: any) => {
+export const mock = ({ fName, contract, mockReturn }: IMockFunctionArgs) => {
   assert(fName in contract.methods, "Function does not exist in contract.");
 
   const mockRes = sinon.mock().returns(mockReturn)();
--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,48 +1,76 @@
 import { expect } from "chai";
 import Web3 from "web3";
-import { clearMocks, mock, run } from "../src/app";
+import { clearMocks, IMockFunctionArgs, IRunFunctionArgs, mock, run } from "../src/app";
 import { address, ERC20TransferABI } from "./mocks/sampleContractABI";
 
 const web3 = new Web3("https://cloudflare-eth.com");
 const testContract = new web3.eth.Contract(ERC20TransferABI, address);
 
 describe("Contract functions", () => {
+  const testReturnValue = () => {
+    return "myReturnValue";
+  };
+
+  const baseMockFunctionArgs: IMockFunctionArgs = {
+    contract: testContract,
+    fName: "transfer",
+    mockReturn: testReturnValue,
+  };
+
+  const baseRunFunctionArgs: IRunFunctionArgs = {
+    fName: "transfer",
+  };
+
   afterEach(() => {
     clearMocks();
   });
 
   it("Can mock contract function and return mocked value", () => {
-    mock(testContract, "transfer", () => {
-      return "myReturnValue";
-    });
-
-    expect(run("transfer")).to.be.equal("myReturnValue");
+    mock(baseMockFunctionArgs);
+    expect(run(baseRunFunctionArgs)).to.be.equal("myReturnValue");
   });
 
   it("Can override contract function mock and return updated mocked value", () => {
-    mock(testContract, "transfer", () => {
-      return "myReturnValue";
-    });
-    mock(testContract, "transfer", () => {
-      return "myUpdatedReturnValue";
-    });
+    mock(baseMockFunctionArgs);
 
-    expect(run("transfer")).to.be.equal("myUpdatedReturnValue");
+    const updatedReturnValue = () => {
+      return "myUpdatedReturnValue";
+    };
+    mock({ ...baseMockFunctionArgs, mockReturn: updatedReturnValue });
+
+    expect(run(baseRunFunctionArgs)).to.be.equal("myUpdatedReturnValue");
   });
 
   it("Fails when attempting to run function that hasn't been mocked yet", () => {
     expect(() => {
-      run("transfer");
+      run(baseRunFunctionArgs);
     }).to.throw();
   });
 
   it("Mocked function can be more complex and affect wider scope", () => {
     let num = 1;
-    mock(testContract, "transfer", () => {
+
+    const widerScopeFunction = () => {
       num++;
+      return num;
+    };
+    mock({ ...baseMockFunctionArgs, mockReturn: widerScopeFunction });
+
+    run(baseRunFunctionArgs);
+    expect(num).to.be.equal(2);
+  });
+
+  it("Can pass optional arguments to mocked function", () => {
+    const mockReturnWithArgs = () => {
+      return "pong";
+    };
+
+    mock({
+      ...baseMockFunctionArgs,
+      mockReturn: mockReturnWithArgs,
+      withArgs: "ping",
     });
 
-    run("transfer");
-    expect(num).to.be.equal(2);
+    expect(run(baseRunFunctionArgs)).to.be.equal("pong");
   });
 });
