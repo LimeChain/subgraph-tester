b29e9ec845980b5d7338bced390c36bddfaeeb2f
Petko <pavlovskipetko@abv.bg>
2021-04-08 13:54:40 +0300

    Add events to mock ABI.

--- tests/mocks/sampleContractABI.ts
+++ tests/mocks/sampleContractABI.ts
@@ -71,4 +71,48 @@ export const ERC20TransferABI: IAbiItem[] = [
     stateMutability: "view",
     type: "function",
   },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        name: "spender",
+        type: "address",
+      },
+      {
+        indexed: false,
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        name: "from",
+        type: "address",
+      },
+      {
+        indexed: true,
+        name: "to",
+        type: "address",
+      },
+      {
+        indexed: false,
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
 ];
