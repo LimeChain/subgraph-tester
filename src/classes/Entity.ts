b919e65965e849f6531535da3f0d2ede38c91a08
Petko <pavlovskipetko@abv.bg>
2021-04-12 16:41:11 +0300

    Params are not mandatory.

--- src/classes/Entity.ts
+++ src/classes/Entity.ts
@@ -2,7 +2,7 @@ export default class Entity {
     public id: string;
     public params: any;
 
-    constructor(id: string, params: any) {
+    constructor(id: string, params?: any) {
         this.id = id;
         this.params = params || {};
     }
