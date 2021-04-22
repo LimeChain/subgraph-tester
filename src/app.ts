0a910089ad2d561f8b87b72473e8c840df586e21
Petko <pavlovskipetko@abv.bg>
2021-04-22 22:04:19 +0300

    Get ready for npm

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import Entity from "./classes/Entity";
+import Event from "./classes/Event";
+import MockContract from "./classes/MockContract";
+import Resolver from "./classes/Resolver";
+import Store from "./classes/Store";
+import SubgraphData from "./classes/SubgraphData";
+
+module.exports = {
+    entity: Entity,
+    event: Event,
+    mockContract: MockContract,
+    relsover: Resolver,
+    store: Store,
+    subgraphData: SubgraphData,
+};
