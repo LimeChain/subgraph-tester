3e00ded751a7fc3b6f7a88910b842065f6ed6d19
Petko <pavlovskipetko@abv.bg>
2021-04-23 15:56:42 +0300

    bundle all host functions so far into one export

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,5 @@
-import BigDecimal from "./classes/BigDecimal";
-import bigInt from "./classes/BigInt";
-import DataSource from "./classes/DataSource";
-import Entity from "./classes/Entity";
-import Ethereum from "./classes/Ethereum";
-import Event from "./classes/Event";
-import MockContract from "./classes/MockContract";
-import Resolver from "./classes/Resolver";
-import Store from "./classes/Store";
-import SubgraphData from "./classes/SubgraphData";
-import TypeConversion from "./classes/TypeConversion";
+import HostImports from "./classes/HostImports";
 
 module.exports = {
-  bigDecimal: BigDecimal,
-  bigInt,
-  dataSource: DataSource,
-  entity: Entity,
-  ethereum: Ethereum,
-  event: Event,
-  mockContract: MockContract,
-  relsover: Resolver,
-  store: Store,
-  subgraphData: SubgraphData,
-  typeConversion: TypeConversion,
+  hostImports: HostImports,
 };
--- src/classes/BigInt.ts
+++ src/classes/BigInt.ts
@@ -1,19 +1,19 @@
 // tslint:disable-next-line: class-name
 export default class bigInt implements BigInt {
     public [Symbol.toStringTag]: "BigInt";
-    private value: bigint;
+    private value: bigint | undefined;
 
-    constructor(value: bigint) {
+    constructor(value?: bigint) {
         this.value = value;
     }
     public toString = (radix?: number): string => {
-        return this.value.toString();
+        return this.value!.toString();
     }
     public toLocaleString = (locales?: string, options?: any): string => {
-        return this.value.toString();
+        return this.value!.toString();
     }
     public valueOf = (): bigint => {
-        return this.value;
+        return this.value!;
     }
     public times = (x: BigInt, y: BigInt): BigInt => {
         return x.valueOf() * y.valueOf();
